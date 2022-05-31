a7cdc7b5477417c9697590d547a157996e93f323
davquev <ingdavquev@gmail.com>
2022-05-31 08:23:44 -0500

    py StartUp Generator

--- app_mystartupname/lib/main.dart
+++ app_mystartupname/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //final wordPair = WordPair.random();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: RandomWord(),
     );
   }
@@ -26,6 +27,9 @@ class _RandomWordState extends State<RandomWord> {
   final List<WordPair> suggestions = <WordPair>[];
   final TextStyle biggerFont = TextStyle(fontSize: 16);
 
+  //vamos a crear una var de tipo "Set"
+  final Set<WordPair> saved = Set<WordPair>();
+
   @override
   Widget build(BuildContext context) {
     //final wordPair = WordPair.random();
@@ -34,6 +38,9 @@ class _RandomWordState extends State<RandomWord> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Name StartUp Gen"),
+        actions: <Widget>[
+          IconButton(onPressed: pushSaved, icon: Icon(Icons.library_books_outlined))
+        ],
       ),
       body: buildSggestions(),
     );
@@ -60,11 +67,59 @@ class _RandomWordState extends State<RandomWord> {
   }
 
   Widget buildRow(WordPair pair) {
+    //Var q controla el color del icono fav
+    final bool alreadySaved = saved.contains(pair);
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          //alreadySaved ? saved.remove(pair) : saved.add(pair),
+          if (alreadySaved){
+            saved.remove(pair);
+          }
+          else{
+            saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+          builder: (BuildContext context){
+            final Iterable<ListTile> tiles = saved.map(
+                (WordPair pair){
+                  return ListTile(
+                    title: Text(
+                      pair.asPascalCase,
+                      style: biggerFont,
+                    ),
+                  );
+                },
+            );
+            final List<Widget> divided = ListTile.divideTiles(
+                context: context,
+                tiles: tiles,
+            ).toList();
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Saved suggestions!!!"),
+              ),
+              body: ListView(
+                children: divided,
+              ),
+            );
+          })
     );
   }
 }
